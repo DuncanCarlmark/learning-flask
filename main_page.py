3be7b85095df63c09262a2ecb81ef27c7cba681d
Duncan Carlmark <36279693+DuncanCarlmark@users.noreply.github.com>
2021-01-14 17:26:32 -0800

    Set up

---
+++ main_page.py
@@ -0,0 +1,8 @@
+import spotipy
+import os
+
+def load_html():
+    client_id = 'e6be6a0e60124f36ad99038de2f36e91'
+    client_secret = '14116a664bd84048a0c7c3004edc9726'
+
+    redirect_uri = 'https://google.com/'
