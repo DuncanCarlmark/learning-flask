ae35dd0fbdeefcd61e7bdee46c6a27a2aa9d1ebd
Duncan Carlmark <36279693+DuncanCarlmark@users.noreply.github.com>
2021-01-18 21:59:55 -0800

    Added code to pull data from url

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, request
+import pandas as pd
+import requests
+from io import StringIO
+
 # Page imports
 import main_page, callback_page
 
@@ -7,6 +11,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def idex():
+
+    orig_url = 'https://drive.google.com/file/d/1N4ZFdyvULQ0gQDxGsvMFURdiaV94y1F_/view?usp=sharing'
+
+    columns = []
+    file_id = orig_url.split('/')[-2]
+    dwn_url='https://drive.google.com/uc?export=download&id=' + file_id
+    url = requests.get(dwn_url).text
+    csv_raw = StringIO(url)
+    dfs = pd.read_csv(csv_raw, sep = '\t')
+    print("SHIT GOT LOADED POGGERS")
+
+
     return main_page.load_html()
 
 @app.route('/callback/')
@@ -14,7 +30,10 @@ def callback():
     code = request.args.get('code')
     print("WE GOT DA CODE")
     print(code)
+
+    sp_auth = ()
     return callback_page.load_html()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
