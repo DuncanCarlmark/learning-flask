44a748181518b0b7760c1cf79636221ed7f8020b
Duncan Carlmark <36279693+DuncanCarlmark@users.noreply.github.com>
2021-01-27 03:08:18 -0800

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -32,38 +32,24 @@ def load_html():
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="A layout example that shows off a responsive product landing page.">
-        <title>Landing Page &ndash; Layout Examples &ndash; Pure</title>
+        <meta name="description" content="Website for DSC180b capstone project in recommender systems">
+        <title>Capstone Recommender</title>
     </head>
     <body>
 
-    <div class="header">
-        <div class="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
-            <a class="pure-menu-heading" href="">Your Site</a>
-
-            <ul class="pure-menu-list">
-                <li class="pure-menu-item pure-menu-selected"><a href="#" class="pure-menu-link">Home</a></li>
-                <li class="pure-menu-item"><a href="#" class="pure-menu-link">Tour</a></li>
-                <li class="pure-menu-item"><a href="#" class="pure-menu-link">Sign Up</a></li>
-            </ul>
-        </div>
-    </div>
-
-    <div class="splash-container">
-        <div class="splash">
-            <h1 class="splash-head">All Capstone No Cap</h1>
-            <p class="splash-subhead">
+    <div>
+        <div>
+            <h1>All Capstone No Cap</h1>
+            <p>
                 Finding music that parents can listen to with their children
             </p>
             <p>
-                <a href="{}" class="pure-button pure-button-primary">Get Started</a>
-            </p>
-
-           
+                <a href="{}" >Get Started</a>
+            </p>     
         </div>
     </div>
     </body>
-</html>
+    </html>
      """.format(auth_url)
 
     return page_html
