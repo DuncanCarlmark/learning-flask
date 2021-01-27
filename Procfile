84b97956b188738861af17a659993fb067c267ff
Duncan Carlmark <36279693+DuncanCarlmark@users.noreply.github.com>
2021-01-26 21:21:28 -0800

    Added script and additional worker to download data

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn app:app
+worker: python download_training.py
--- main_page.py
+++ main_page.py
@@ -23,28 +23,8 @@ def load_html():
     # Force auth every time
     auth_url = sp_oauth.get_authorize_url()
 
-    # client = boto3.client('s3')
-
-    # path_user_profile = 's3://capstone-training-data/user_profile.tsv'
-    # path_user_artists = 's3://capstone-training-data/user_artist.tsv'
-
-    # cols_user_profile = ['user_id', 'gender', 'age', 'country', 'date']
-    # cols_user_artist = ['user_id', 'artist_id', 'artist_name', 'plays']
-
-    # print('Downloading user_profile')
-    # user_profile = pd.read_csv(path_user_profile,
-    #                       sep = '\t',
-    #                       names = cols_user_profile)
-
-    # print('Downloading user_artist')
-    # user_artist = pd.read_csv(path_user_artists,
-    #                      sep = '\t',
-    #                      names = cols_user_artist)
-
-    # user_profile.to_csv('user_profile.csv')
-    # user_artist.to_csv('user_artist.csv')
-
-     user_profile = pd.read_csv('user_profile.tsv',
+    # P
+    user_profile = pd.read_csv('user_profile.tsv',
                           sep = '\t',
                           names = cols_user_profile)
 
